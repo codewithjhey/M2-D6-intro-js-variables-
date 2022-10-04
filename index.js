35aa1279b755ca067b7c866b329f7ee5a5868d9e
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-10-04 14:06:54 +0100

    Understanding Variables in JS completed

--- index.js
+++ index.js
@@ -8,5 +8,14 @@ let space = " "
 
 console.log("----------- Name decoration starts here!!!-----------")
 // Sum of variables into full name
+let fullName = forNavn + space + efterNavn
+console.log("Hey! My name is:", space + forNavn + space + efterNavn)
+//Sum of Given name including Middle name
+let givenName = forNavn + space + mellemNavn + space + efterNavn
+console.log("Hey! My Given Name is:", givenName)
 
-console.log(forNavn + space + efterNavn)
+//Calculating my birthyear using Js and saving to the variable.
+let myAge = 65
+let currentYear = 2022
+let birthYear = currentYear - myAge
+console.log("I was born in the year:", birthYear)
