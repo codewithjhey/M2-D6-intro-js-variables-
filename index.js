688e77a5ffc60c362dc415f14d6e47ed25fc155a
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-10-04 13:29:29 +0100

    Variables wriiten in Danish with assigned values

---
+++ index.js
@@ -0,0 +1,12 @@
+// Introduction to js variables
+// INPUTS:
+// Variables wriiten in Danish with assigned values
+let forNavn = "victor"
+let efterNavn = "oluwole"
+let mellemNavn = "jayson"
+let space = " "
+
+console.log("----------- Name decoration starts here!!!-----------")
+// Sum of variables into full name
+
+console.log(forNavn + space + efterNavn)
